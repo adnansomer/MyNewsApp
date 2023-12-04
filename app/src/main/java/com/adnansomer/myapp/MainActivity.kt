318fe6232712eb0f4c0ac6190dd4000a811ce1bf
a84274753 <adnan.somer2@huawei.com>
2023-12-05 01:40:52 +0300

    Splash Completed

--- app/src/main/java/com/adnansomer/myapp/MainActivity.kt
+++ app/src/main/java/com/adnansomer/myapp/MainActivity.kt
@@ -3,11 +3,13 @@ package com.adnansomer.myapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.adnansomer.myapp.ui.theme.NewsAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             NewsAppTheme {
 
